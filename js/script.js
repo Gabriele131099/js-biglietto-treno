5300452bdbf130cac0925c81593bb87c7464a323
gabriele131099 <gabrisanna131099@gmail.com>
2021-05-29 00:25:27 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 alert("Preventivo prezzo biglietto");
 var km = prompt("Quanti Km vuoi percorrere?");
-var age= prompt("Quanti anni hai?");
-
+var age= prompt("Quanti anni hai?"); 
 var prezzo = km * 0.21;
 var saleMin = prezzo - prezzo/100*20;
 var saleMax = prezzo - prezzo/100*40;
-
-
 if (age < 18){
     document.getElementById("prezzo").innerHTML = saleMin + " €";
 }
@@ -16,9 +13,6 @@ else{ if( age >= 65 ){
 }else
 {
     document.getElementById("prezzo").innerHTML = prezzo + " €";
-
 }
-
-
 }
 
