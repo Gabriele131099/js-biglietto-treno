b9a4a14f0be54909f8bceb7e0dcfb741c1299ada
gabriele131099 <gabrisanna131099@gmail.com>
2021-05-30 23:59:00 +0200

    struttura

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,37 +1,57 @@
 function anno() {
     var age =2021 -  document.getElementById("anno").value  ;
-    var km = 100;
-    var narnia = 0.9;
-    var blade = 0.7;
-    var maze = 0.8;
+    var km = prompt("Quanti km vuoi fare?");
+    var kmBlade = 800;
+    var kmMaze = 600;
+    var kmNarnia = 1000;
+
+
     var prezzo = km * 0.21;
+    var prezzoMaze = kmMaze * 0.21;
+    var prezzoBlade = kmBlade * 0.21;
+    var prezzoNarnia = kmNarnia * 0.21;
+
+
     var saleMin = prezzo - prezzo/100*20;
+    var saleMinBlade = prezzoBlade - prezzoBlade/100*20;
+    var saleMinNarnia = prezzoNarnia - prezzoNarnia/100*20;
+    var saleMinMaze = prezzoMaze - prezzoMaze/100*20;
+
     var saleMax = prezzo - prezzo/100*40;
+    var saleMaxBlade = prezzoBlade - prezzoBlade/100*40;
+    var saleMaxNarnia = prezzoNarnia - prezzoNarnia/100*40;
+    var saleMaxMaze = prezzoMaze - prezzoMaze/100*40;
+
+
+
+
+
+
 
 
     if (age < 18){
         document.getElementById("costo").innerHTML = saleMin + " €";
-        document.getElementById("narnia").innerHTML = saleMin*narnia + " €";
-        document.getElementById("maze").innerHTML = saleMin*maze + " €";
-        document.getElementById("blade").innerHTML = saleMin*blade + " €";
+        document.getElementById("narnia").innerHTML = saleMinNarnia + " €";
+        document.getElementById("maze").innerHTML = saleMinMaze + " €";
+        document.getElementById("blade").innerHTML = saleMinBlade + " €";
 
 
 
     }
     else{ if( age >= 65 ){
         document.getElementById("costo").innerHTML = saleMax + " €";
-        document.getElementById("narnia").innerHTML = saleMax*narnia + " €";
-        document.getElementById("maze").innerHTML = saleMax*maze + " €";
-        document.getElementById("blade").innerHTML = saleMax*blade + " €";
+        document.getElementById("narnia").innerHTML = saleMaxNarnia + " €";
+        document.getElementById("maze").innerHTML = saleMaxMaze + " €";
+        document.getElementById("blade").innerHTML = saleMaxBlade + " €";
 
 
 
     }else
     {
         document.getElementById("costo").innerHTML = prezzo + " €";
-        document.getElementById("narnia").innerHTML = prezzo*narnia + " €";
-        document.getElementById("maze").innerHTML = prezzo*maze + " €";
-        document.getElementById("blade").innerHTML = prezzo*blade + " €";
+        document.getElementById("narnia").innerHTML = prezzoNarnia + " €";
+        document.getElementById("maze").innerHTML = prezzoMaze + " €";
+        document.getElementById("blade").innerHTML = prezzoBlade + " €";
 
 
 
